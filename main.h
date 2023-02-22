82fb017a3f0f924ba372057dd11b049b68e5b391
Mmulobi <mosesmutush7@gmail.com>
2023-02-22 14:18:52 -0800

    add fix

--- main.h
+++ main.h
@@ -17,5 +17,22 @@ void fork_wait_exec();
 char **separate_string(char *s);
 char **create_argv(char *argv);
 char *_getenv(const char *name);
+int num_len(int num);
+char *_itoa(int num);
+int create error(char **args, int err);
+char *fill_path_dir(char *path);
+list_t *get_path_dir(char *path);
+char *get_location(char *command);
+int shellby_alias(char **args, char __attribute__((__unused__)) **front);
+void set_alias(char *var_name, char *value);
+void print_alias(alias_t *alias);
+int shellby_env(char **args, char __attribute__((__unused__)) **front);
+int shellby_setenv(char **args, char __attribute__((__unused__)) **front);
+int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front);
+int (*get_builtin(char *command))(char **args, char **front);
+int shellby_exit(char **args, char **front);
+int shellby_cd(char **args, char __attribute__((__unused__)) **front);
+int shellby_help(char **args, char __attribute__((__unused__)) **front);
+
 
 #endif
