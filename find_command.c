9365831cef882e4a5b0b3b70473a8c8b29cc680e
Daniel Garcia-Briseno <dg.briseno@gmail.com>
2023-03-03 08:53:10 -0500

    Review comments

--- find_command.c
+++ find_command.c
@@ -31,7 +31,12 @@ char *find_command(char *command, struct path *path_head)
 		s = stat(directory, &statbuf);
 		if (s == 0)
 		{
-			free(command);
+            /* Don't free your input parameters */
+            /* free(command); */
+            /* First see my comment in simple.c about double free. */
+            /* The problem with 'free' by itself is that it's hard to "just know" what's being free'd */
+            /* Per my other comment, if you had a _strncat_free then you know that this free is associated with a previous strncat call. */
+            /* Even if it's just a macro for free */
 			free(command2);
 			return (directory);
 		}
@@ -40,7 +45,8 @@ char *find_command(char *command, struct path *path_head)
 	}
 	errno = 2;
 
-	free(command);
+    /* Don't free your input parameters (The exception is if the function is named "free" or "cleanup" or some thing where the caller knows it's going to free the input parameters. This led you to a double free exception. */
+    /* free(command); */
 	free(command2);
 	return (NULL);
 }
--- strncat.c
+++ strncat.c
@@ -3,18 +3,22 @@
  * _strncat - concatenates two strings
  * @str1: string 1
  * @str2: string 2
- * Return: concatenated string
+ * Return: concatenated string that needs to be free'd
  */
 char *_strncat(char *str1, char *str2)
 {
+    /* Same as other "declare at top of function" comments. */
+    /* Declare & assign where they are needed */
 	char *dest = NULL;
 	int len = 0, b = 0, n = 0;
 
+    /* Why not use strlen? Is that because it's not listed in limitations.txt? */
 	for (n = 0; str1[n] != '\0'; n++)
 	{
 		len++;
 	}
 
+    /* You could have wrote your own strlen like you wrote your own strcat */
 	for (n = 0; str2[n] != '\0'; n++)
 	{
 		len++;
@@ -28,7 +32,7 @@ char *_strncat(char *str1, char *str2)
 		exit(EXIT_FAILURE);
 	}
 
-
+    /* same comment as above, but with strcpy */
 	for (n = 0, b = 0; str1[n] != '\0'; n++, b++)
 	{
 		dest[b] = str1[n];
